395065cd122c2d3a94a2de461985a1f93b3dcdba
ACuteWoof <sutharshanvithushan@gmail.com>
2021-10-06 17:04:37 +0530

    cd test

--- buildscript.sh
+++ buildscript.sh
@@ -6,15 +6,8 @@ cd /root/
 #Clone the lhos repo
 git clone https://github.com/woof-os/iso-profile 
 
-ls
-
-#Go to this new working folder
-cd iso-profile 
-
-ls
-
 #Run the script to build the ISO
-./build.sh
+/root/iso-profile/build.sh
 
 #Move the final ISO to /tmp to be accessible from the host
-mv out/*.iso /tmp
+mv /root/iso-profile/out/*.iso /tmp
