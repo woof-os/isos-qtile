040b3e500899e9757ff52b713a9bbdc011f81f28
ACuteWoof <sutharshanvithushan@gmail.com>
2021-10-06 18:15:02 +0530

    cd-ing

--- buildscript.sh
+++ buildscript.sh
@@ -9,8 +9,9 @@ git clone https://github.com/woof-os/iso-profile /root/iso-profile
 #chmod the script
 chmod +x /root/iso-profile/build.sh
 
-#Run the script to build the ISO
-/root/iso-profile/build.sh
+#CD and run the script to build the ISO
+cd /root/iso-profile/
+build.sh
 
 #Move the final ISO to /tmp to be accessible from the host
 mv /root/iso-profile/out/*.iso /tmp
